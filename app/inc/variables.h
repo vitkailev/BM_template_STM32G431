c55fd2d421a58fb53e1b539e102c970b143d2ac2
Vitali Kailevich <vit.kailev@gmail.com>
2024-01-14 01:00:34 +0100

    Create variables.h file

---
+++ app/inc/variables.h
@@ -0,0 +1,16 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // VARIABLES_H
