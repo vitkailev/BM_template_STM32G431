ae890ac3af305b5e3f5febe07a2119f6c9634a08
Vitali Kailevich <vit.kailev@gmail.com>
2024-01-18 01:29:20 +0100

    Add digital pins code initialization. Add the button control code

--- app/src/main.c
+++ app/src/main.c
@@ -1,6 +1,7 @@
 #include "stm32g4xx_hal.h"
 
 #include "settings.h"
+#include "functions.h"
 
 MCUDef Mcu;
 
@@ -19,6 +20,22 @@ int main(void) {
 }
 
 static int initPeripheral(void) {
+
+    Mcu.button.duration = 50; // ms
+    Mcu.button.pin = GPIO_PIN_13;
+    Mcu.button.obj = GPIOC;
+    Mcu.led.pin = GPIO_PIN_5;
+    Mcu.led.obj = GPIOA;
+    Mcu.oscPins[OSC_CHANNEL_1].pin = GPIO_PIN_10; // D2
+    Mcu.oscPins[OSC_CHANNEL_1].obj = GPIOA;
+    Mcu.oscPins[OSC_CHANNEL_2].pin = GPIO_PIN_5; // D4
+    Mcu.oscPins[OSC_CHANNEL_2].obj = GPIOB;
+    Mcu.oscPins[OSC_CHANNEL_3].pin = GPIO_PIN_8; // D7
+    Mcu.oscPins[OSC_CHANNEL_3].obj = GPIOA;
+    Mcu.oscPins[OSC_CHANNEL_4].pin = GPIO_PIN_9; // D8
+    Mcu.oscPins[OSC_CHANNEL_4].obj = GPIOA;
+    Mcu.timer_8kHz.freq = 8000; // Hz
+
     initialization(&Mcu);
     return 0;
 }
@@ -26,5 +43,14 @@ static int initPeripheral(void) {
 static void application(void) {
     if (Mcu.flags.isSysTickTriggered) {
         Mcu.flags.isSysTickTriggered = false;
+
+        Mcu.runtime++;
+        checkPinState(&Mcu.button);
+    }
+
+    if (isPinTriggered(&Mcu.button)) {
+        Mcu.button.isTriggered = false;
+
+        changePinState(&Mcu.led, !getPinState(&Mcu.led));
     }
 }
