e0549fde2ddd6b2fd059605f8ef966a494ea9138
Vitali Kailevich <vit.kailev@gmail.com>
2024-12-26 18:13:48 +0100

    Update the initPeripheral function (add PWM and the generator "start" functions)

--- app/src/main.c
+++ app/src/main.c
@@ -18,7 +18,7 @@ int main(void) {
     __HAL_RCC_CLEAR_RESET_FLAGS();
 
     while (true) {
-        HAL_IWDG_Refresh((IWDG_HandleTypeDef *) Mcu.handlers.wdt);
+        HAL_IWDG_Refresh((IWDG_HandleTypeDef *) Mcu.handles.wdt);
 
         application();
     }
@@ -85,27 +85,34 @@ static void application(void) {
 }
 
 static int initPeripheral(void) {
-
     Mcu.led[LED_GREEN].pin = GPIO_PIN_5;
-    Mcu.led[LED_GREEN].obj = GPIOA;
+    Mcu.led[LED_GREEN].handle = GPIOA;
     Mcu.button[BUTTON_1].duration = 50; // ms
     Mcu.button[BUTTON_1].pin = GPIO_PIN_13;
-    Mcu.button[BUTTON_1].obj = GPIOC;
+    Mcu.button[BUTTON_1].handle = GPIOC;
     Mcu.oscPins[OSC_CHANNEL_1].pin = GPIO_PIN_10; // D2
-    Mcu.oscPins[OSC_CHANNEL_1].obj = GPIOA;
+    Mcu.oscPins[OSC_CHANNEL_1].handle = GPIOA;
     Mcu.oscPins[OSC_CHANNEL_2].pin = GPIO_PIN_5; // D4
-    Mcu.oscPins[OSC_CHANNEL_2].obj = GPIOB;
+    Mcu.oscPins[OSC_CHANNEL_2].handle = GPIOB;
     Mcu.oscPins[OSC_CHANNEL_3].pin = GPIO_PIN_8; // D7
-    Mcu.oscPins[OSC_CHANNEL_3].obj = GPIOA;
+    Mcu.oscPins[OSC_CHANNEL_3].handle = GPIOA;
     Mcu.oscPins[OSC_CHANNEL_4].pin = GPIO_PIN_9; // D8
-    Mcu.oscPins[OSC_CHANNEL_4].obj = GPIOA;
+    Mcu.oscPins[OSC_CHANNEL_4].handle = GPIOA;
 
-    if (initialization(&Mcu) == SETTING_SUCCESS) {
+    UART_init(&Mcu.uart1);
+    UART_init(&Mcu.uart2);
+
+    if (SETTING_SUCCESS == initialization(&Mcu)) {
         setCompThresholdLevel(&Mcu.comp, 2137);
-        HAL_COMP_Start((COMP_HandleTypeDef *) Mcu.comp.handler);
+        HAL_COMP_Start((COMP_HandleTypeDef *) Mcu.comp.handle);
 
-        UART_init(&Mcu.uart1);
-        UART_init(&Mcu.uart2);
+        HAL_TIM_Base_Start_IT((TIM_HandleTypeDef *) Mcu.measTimer.handle);
+
+        HAL_TIM_PWM_Start((TIM_HandleTypeDef *) Mcu.pwmTimer.handle, TIM_CHANNEL_1);
+        HAL_TIMEx_PWMN_Start((TIM_HandleTypeDef *) Mcu.pwmTimer.handle, TIM_CHANNEL_1);
+        setPWMDutyCycle(&Mcu.pwmTimer, TIM_CHANNEL_1, 15);
+
+        changeGeneratorMode(&Mcu.generator, GENERATE_SAWTOOTH);
     }
     return 0;
 }
